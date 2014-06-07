c4e343860b71ab4ff3d91a63b2cfbac384fa5c49
nkichev <nkichev@abv.bg>
2014-06-07 21:30:45 +0300

    Enemies
    Sample enemy added

---
+++ SuperMario/js/enemies.js
@@ -0,0 +1,30 @@
+﻿var enemyLayer = new Kinetic.Layer();
+
+var enemyImageObj = new Image();
+enemyImageObj.onload = function () {
+    var mushroom = new Kinetic.Sprite({
+        x: 960,
+        y: 512,
+        image: enemyImageObj,
+        animation: 'stay',
+        animations: {
+            stay: [
+              // x, y, width, height (2 frames)
+              0, 6, 29, 26,
+              55, 6, 29, 26, 
+            ]
+        },
+        frameRate: 7,
+        frameIndex: 0
+    });
+
+    // add the shape to the layer
+    enemyLayer.add(mushroom);
+
+    // add the layer to the stage
+    stage.add(enemyLayer);
+
+    // start sprite animation
+    mushroom.start();
+};
+enemyImageObj.src = 'Images/smb_enemies_sheet.png';
--- SuperMario/js/mario.js
+++ SuperMario/js/mario.js
@@ -1,59 +1,59 @@
-﻿    var layer = new Kinetic.Layer();
+﻿var marioLayer = new Kinetic.Layer();
 
-    var imageObj = new Image();
-    imageObj.onload = function () {
-        var mario = new Kinetic.Sprite({
-            x: 10,
-            y: 390,
-            image: imageObj,
-            animation: 'stay',
-            animations: {
-                walk: [
-                  // x, y, width, height (2 frames)
-                  0, 0, 50, 150,
-                  50, 0, 50, 150
-                ],
-                stay: [
-                    0, 0, 50, 150
-                ],
-                jump: [
-                    100, 70, 50, 150
-                ]
-            },
-            frameRate: 7,
-            frameIndex: 0
-        });
+var marioImageObj = new Image();
+marioImageObj.onload = function () {
+    var mario = new Kinetic.Sprite({
+        x: 10,
+        y: 390,
+        image: marioImageObj,
+        animation: 'stay',
+        animations: {
+            walk: [
+              // x, y, width, height (2 frames)
+              0, 0, 50, 150,
+              50, 0, 50, 150
+            ],
+            stay: [
+                0, 0, 50, 150
+            ],
+            jump: [
+                100, 70, 50, 150
+            ]
+        },
+        frameRate: 7,
+        frameIndex: 0
+    });
 
-        layer.add(mario);
+    marioLayer.add(mario);
 
-        stage.add(layer);
+    stage.add(marioLayer);
 
-        mario.start();
+    mario.start();
 
-        var frameCount = 0;
+    var frameCount = 0;
 
-        mario.on('frameIndexChange', function (evt) {
-            if ((mario.animation() === 'walk') && ++frameCount > 2) {
-                mario.animation('stay');
-                frameCount = 0;
-            }
-            else if ((mario.animation() === 'jump') && ++frameCount > 2) {
-                mario.animation('stay');
-                frameCount = 0;
-            }
-        });
-
-        function onKeyDown(ev) {
-            if (ev.keyCode === 39) {
-                mario.animation('walk');
-            } else if (ev.keyCode === 38) {
-                mario.animation('jump');
-            } else if (ev.keyCode === 40) {
-                mario.animation('stay');
-            }
+    mario.on('frameIndexChange', function (evt) {
+        if ((mario.animation() === 'walk') && ++frameCount > 2) {
+            mario.animation('stay');
+            frameCount = 0;
         }
+        else if ((mario.animation() === 'jump') && ++frameCount > 2) {
+            mario.animation('stay');
+            frameCount = 0;
+        }
+    });
 
-        document.body.addEventListener('keydown', onKeyDown, false);
+    function onKeyDown(ev) {
+        if (ev.keyCode === 39) {
+            mario.animation('walk');
+        } else if (ev.keyCode === 38) {
+            mario.animation('jump');
+        } else if (ev.keyCode === 40) {
+            mario.animation('stay');
+        }
+    }
 
-    };
-    imageObj.src = 'Images/mario1.png';
+    document.body.addEventListener('keydown', onKeyDown, false);
+
+};
+marioImageObj.src = 'Images/mario1.png';
