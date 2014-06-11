268330babfd4e6209e8178a462572bac6057930b
valkanov <deyan.valkanov@gmail.com>
2014-06-11 14:50:14 +0300

    Added highscores

--- SuperMario/js/lives.js
+++ SuperMario/js/lives.js
@@ -5,22 +5,22 @@ var remainingLives = startLiveCount;
 var lives = new Kinetic.Layer();
 
 var livesTextField = new Kinetic.Text({
-    x: 580,
-    y: 10,
-    text: "LIVES",
-    fontSize: 20,
-    fontFamily: 'Arial Black',
-    fill: 'white'
-});
+                                          x: 580,
+                                          y: 10,
+                                          text: "LIVES",
+                                          fontSize: 20,
+                                          fontFamily: 'Arial Black',
+                                          fill: 'white'
+                                      });
 
 var remainingLivesField = new Kinetic.Text({
-    x: 605,
-    y: 30,
-    text: remainingLives,
-    fontSize: 20,
-    fontFamily: 'Arial Black',
-    fill: 'white'
-});
+                                               x: 605,
+                                               y: 30,
+                                               text: remainingLives,
+                                               fontSize: 20,
+                                               fontFamily: 'Arial Black',
+                                               fill: 'white'
+                                           });
 
 lives.add(livesTextField);
 lives.add(remainingLivesField);
@@ -28,11 +28,39 @@ lives.add(remainingLivesField);
 stage.add(lives);
 
 function die() {
-    //TODO
     remainingLives -= 1;
+    if (remainingLives == 0) {
+        gameOver();
+    }
+    //TODO
+    //define die
 }
 
-function dieThreeTimes() {
+function gameOver() {
+
+    //highscores
+    var userName = prompt('Enter your name: ', 'unnamed');
+    var currentScores = localStorage.getItem('scores');
+    if (!currentScores) {
+        currentScores = [];
+    } else {
+        currentScores = JSON.parse(currentScores);
+    }
+
+    if (currentScores.length == 0) {
+        currentScores.push({ name: userName, score: mario.score });
+    } else {
+        if (mario.score < currentScores[currentScores.length - 1].score) {
+            currentScores.push({ name: userName, score: mario.score });
+        } else {
+            for (var i = 0; i < currentScores.length; i++) {
+                if (currentScores[i].score < mario.score) {
+                    currentScores.splice(i, 0, { name: userName, score: mario.score });
+                    break;
+                }
+            }
+        }
+    }
     //TODO
-    //GAME OVER
+    //get the mario scores and write highscores on the screen 
 }
