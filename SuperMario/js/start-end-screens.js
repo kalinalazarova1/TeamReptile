4c5d102743bfad23e6b770c5008a0aedb57f8813
valkanov <deyan.valkanov@gmail.com>
2014-06-14 14:54:04 +0300

    fix start screen

---
+++ SuperMario/js/start-end-screens.js
@@ -0,0 +1,24 @@
+﻿var screens = new Kinetic.Layer();
+
+var startScreen = new Image();
+
+startScreen.onload = function () {
+    var start = new Kinetic.Image({
+        x: 100,
+        y: 30,
+        image: startScreen,
+        width: 400,
+        height: 400
+    });
+
+    // add the shape to the layer
+    screens.add(start);
+
+    // add the layer to the stage
+    stage.add(screens);
+
+    setInterval(function () {
+        screens.remove(startScreen);
+    }, 4000);
+};
+startScreen.src = 'Images/screens/screen-start.png';
